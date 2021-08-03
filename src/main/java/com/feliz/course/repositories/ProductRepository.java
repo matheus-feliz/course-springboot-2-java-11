4b63fe4d15e97f8a2aa432e955b5855ee09ed6ae
matheus-feliz <matheusfoliveira172@gmail.com>
2021-08-03 19:56:06 -0300

    Product

---
+++ src/main/java/com/feliz/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.feliz.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.feliz.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
